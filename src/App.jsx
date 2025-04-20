12a64beea3bf584c9ec7421dc90a37121f071e06
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-04-20 21:35:34 +0200

    Social App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import {Person} from '@mui/icons-material'
 import './App.css'
 // import Home from './pages/home/home'
+import Topbar from './components/topbar/Topbar'
 function App() {
   
   return (
     < >
    
      
-      <Person/> 
+      <Topbar/> 
     </>
   )
 }
